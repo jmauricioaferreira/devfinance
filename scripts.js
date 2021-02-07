1fe6ffbc2a5cc7ba7a6ee1cfd43bb5c24633e74d
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2021-02-07 17:55:07 -0300

    creating formatCurrency function

---
+++ scripts.js
@@ -0,0 +1,81 @@
+const Modal = {
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
+
+const transactions = [
+  {
+    id: 1,
+    description: "Luz",
+    amount: -50000,
+    date: "13/01/2021",
+  },
+  {
+    id: 2,
+    description: "Website",
+    amount: 5000,
+    date: "13/01/2021",
+  },
+  {
+    id: 2,
+    description: "Internet",
+    amount: -2000,
+    date: "13/01/2021",
+  },
+];
+
+const Transaction = {
+  incomes() {},
+  expenses() {},
+  total() {},
+};
+
+const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+    DOM.transactionsContainer.appendChild(tr);
+  },
+
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+    `;
+    return html;
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
